bdc6bf2d3b022d75313600c9c8b50eef0b4548f4
sourav-khunger <khungersourav30@gmail.com>
2019-04-22 18:52:01 +0530

    just added a demo button

--- app/src/main/java/com/doozycod/edutek/MainActivity.java
+++ app/src/main/java/com/doozycod/edutek/MainActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
     RelativeLayout submit_btn;
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
